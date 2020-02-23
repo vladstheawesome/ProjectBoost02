527e48eccb812afb57969d6fdc0e86ea97a57e80
vladstheawesome <vmabhena@gmail.com>
2020-02-23 18:47:27 +0000

    Moving Objects

---
+++ Assets/Scripts/Jetpack.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jetpack : MonoBehaviour
+{
+    Rigidbody rigidBody;
+    AudioSource jetpackSound;
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainThrust = 100f;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        jetpackSound = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        Thrust();
+        Rotate();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                {
+                    print("safe");
+                }
+                break;
+            default:
+                {
+                    print("dead");
+                }
+                break;
+        }
+    }
+
+    private void Thrust()
+    {
+        //float thisThrust = mainThrust * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.Space)) // can thrust while rotating
+        {
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+            if (!jetpackSound.isPlaying)
+            {
+                jetpackSound.Play();
+            }
+        }
+        else
+        {
+            jetpackSound.Stop();
+        }
+    }
+
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true; // take manual control of rotation
+
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * rotationThisFrame);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
+        }
+
+        rigidBody.freezeRotation = false; // resume physics
+    }    
+}
---
+++ Assets/Scripts/Oscillator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Oscillator : MonoBehaviour
+{
+    [SerializeField] Vector3 movementVector = new Vector3(10f, 10f, 10f);
+    [SerializeField] float period = 3f;
+
+    // todo remove from inspectore later
+    /*[Range(0,1)] [SerializeField]*/ float movementFactor;
+
+    Vector3 startingPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (period <= Mathf.Epsilon)
+        {
+            return;
+        }
+
+        float cycles = Time.time / period;                
+
+        const float tau = Mathf.PI * 2f;
+        float rawSinWave = Mathf.Sin(cycles * tau); // goes from -1 to +1
+
+        movementFactor = rawSinWave / 2f + 0.5f;
+        Vector3 offset = movementFactor * movementVector;
+        transform.position = startingPos + offset;
+    }
+}
