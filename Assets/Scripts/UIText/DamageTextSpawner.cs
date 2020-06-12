9430d6e6d86e5dad12836f5f6f94a1b2dde3da76
vladstheawesome <vmabhena@gmail.com>
2020-06-12 22:58:01 +0100

    Glowing Particles V 1

---
+++ Assets/Scripts/UIText/DamageTextSpawner.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ProjectBoost.UIText
+{
+    public class DamageTextSpawner : MonoBehaviour
+    {
+        [SerializeField] DamageText damageTextPrefab = null;
+
+        public void Spawn(float damageAmount)
+        {
+            DamageText instance = Instantiate<DamageText>(damageTextPrefab, transform);
+            instance.SetValue(damageAmount);
+        }
+    }
+}
